17c52fe56991ddfeedac4847bb10b4e2dea18ba8
elinwonyoungPARK <elinpark.102@gmail.com>
2020-12-07 10:17:53 +0900

    2020-12-07

---
+++ src/main/java/com/example/demo/impls/ArticleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.impls;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl {
+    
+}
---
+++ src/main/java/com/example/demo/utils/Printer.java
@@ -0,0 +1,9 @@
+package com.example.demo.utils;
+
+import java.util.function.Consumer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Printer{
+
+}
