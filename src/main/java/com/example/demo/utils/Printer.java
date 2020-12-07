b5a3ffcd1a233dabb5ce71df3de456a2ceba67d7
elinwonyoungPARK <elinpark.102@gmail.com>
2020-12-07 14:58:19 +0900

    14

--- src/main/java/com/example/demo/utils/Printer.java
+++ src/main/java/com/example/demo/utils/Printer.java
@@ -1,9 +1,8 @@
 package com.example.demo.utils;
 
-import java.util.function.Consumer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Printer{
-
+    public void accept(Object c)
 }
