8000e0e5603f6863594083a897f0598d0eecafdd
elinwonyoungPARK <elinpark.102@gmail.com>
2020-12-07 15:05:00 +0900

    15

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import com.example.demo.utils.Printer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/demo/utils/Printer.java
+++ src/main/java/com/example/demo/utils/Printer.java
@@ -4,5 +4,4 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Printer{
-    public void accept(Object c)
 }
